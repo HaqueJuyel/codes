3874dc3ab8cfe7c8c6084375fcf54c102c940170
Mohammed Juyel Haque <mohammedjuyelh@gmail.com>
2025-08-23 15:30:42 +0530

    cicd

--- aws-terraform-cicd/terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terrform-backend-b"   # <<-- change me
-    key            = "terraform/terraform.tfstate"
-    region         = "us-east-1"                   # <<-- change me
-    dynamodb_table = "terraform-state-lock"        # <<-- change me
-    encrypt        = true
-  }
-}
