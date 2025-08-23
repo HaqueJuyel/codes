7ded25176c33e0082227e97647378a6b432e03cc
Mohammed Juyel Haque <mohammedjuyelh@gmail.com>
2025-08-23 14:59:06 +0530

    cicd

--- aws-terraform-cicd/scripts/tf_init.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-from utils import run_cmd
-
-def terraform_init():
-    # init with backend config file already in terraform/backend.tf
-    run_cmd("terraform -chdir=terraform init -input=false -upgrade")
-
-if __name__ == "__main__":
-    terraform_init()
--- aws-terraform-cicd/terraform/backend.tf
+++ aws-terraform-cicd/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"   # <<-- change me
-    key            = "terraform/aws-cicd-ec2/terraform.tfstate"
+    bucket         = "terrform-backend-b"   # <<-- change me
+    key            = "terraform/terraform.tfstate"
     region         = "us-east-1"                   # <<-- change me
     dynamodb_table = "terraform-state-lock"        # <<-- change me
     encrypt        = true
